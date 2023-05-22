c20f0894ca3f3410f55d5d8a631197f9e0bd85e7
andryan <andryan130203@gmail.com>
2023-05-22 08:47:11 +0700

    Initial commit

---
+++ database/migrations/2023_05_19_160557_create_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ticket_id')->constrrained('tickets');
+            $table->date('start_date');
+            $table->string('place',255);
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+};
